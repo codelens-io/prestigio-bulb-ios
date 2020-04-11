e74b6622cd8ac3014e7cc6b85996d5482c35292c
Majki <majki@majki.org>
2020-04-11 11:22:23 +0200

    Initial Commit

---
+++ prestigio-bulb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prestigio-bulb
+//
+//  Created by Majki on 2020. 04. 11..
+//  Copyright © 2020. CodeLens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
